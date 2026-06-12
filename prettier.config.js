82b27a48da6503eefd0621962ea6e0a003f16638
Rafli Malik <Rafli.Malik@gmail.com>
2026-06-12 07:54:02 +0700

    init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+	useTabs: true,
+	printWidth: 100,
+	tabWidth: 2,
+	plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
